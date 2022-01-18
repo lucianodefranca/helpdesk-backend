fe9462f263c03fc6ca8c6240178ddc38b428c5d0
luciano de franca <lucianodefranca86@gmail.com>
2022-01-17 22:57:45 -0300

    Projeto iniciado

---
+++ src/test/java/com/luciano/helpdesk/HelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luciano.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
