5af0473bc203742c9bc8e3f27f793ad0198c5d15
luciano de franca <lucianodefranca86@gmail.com>
2022-01-17 23:44:43 -0300

    Entidade Chamado

---
+++ src/main/java/com/luciano/helpdesk/entities/Chamado.java
@@ -0,0 +1,113 @@
+package com.luciano.helpdesk.entities;
+
+import com.luciano.helpdesk.entities.enums.Prioridade;
+import com.luciano.helpdesk.entities.enums.Status;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Chamado {
+
+    private Integer id;
+    private LocalDateTime dataAbertura;
+    private LocalDateTime dataFechamento;
+    private Integer prioridade;
+    private String observacoes;
+    private Integer status;
+    private Tecnico tecnico;
+    private Cliente cliente;
+
+    public Chamado() {
+        this.dataAbertura = LocalDateTime.now();
+        this.setPrioridade(Prioridade.BAIXA);
+        this.setStatus(Status.ABERTO);
+    }
+
+    public Chamado(Integer id, Prioridade prioridade, String observacoes,
+                   Status status, Tecnico tecnico, Cliente cliente) {
+        this.id = id;
+        this.dataAbertura = LocalDateTime.now();
+        this.prioridade = (prioridade == null) ? 0 : prioridade.getCod();
+        this.observacoes = observacoes;
+        this.status = (status == null) ? 0: status.getCod();
+        this.tecnico = tecnico;
+        this.cliente = cliente;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDataAbertura() {
+        return dataAbertura;
+    }
+
+    public void setDataAbertura(LocalDateTime dataAbertura) {
+        this.dataAbertura = dataAbertura;
+    }
+
+    public LocalDateTime getDataFechamento() {
+        return dataFechamento;
+    }
+
+    public void setDataFechamento(LocalDateTime dataFechamento) {
+        this.dataFechamento = dataFechamento;
+    }
+
+    public Prioridade getPrioridade() {
+        return Prioridade.toEnum(this.prioridade);
+    }
+
+    public void setPrioridade(Prioridade prioridade) {
+        this.prioridade = prioridade.getCod();
+    }
+
+    public String getObservacoes() {
+        return observacoes;
+    }
+
+    public void setObservacoes(String observacoes) {
+        this.observacoes = observacoes;
+    }
+
+    public Status getStatus() {
+        return Status.toEnum(this.status);
+    }
+
+    public void setStatus(Status status) {
+        this.status = status.getCod();
+    }
+
+    public Tecnico getTecnico() {
+        return tecnico;
+    }
+
+    public void setTecnico(Tecnico tecnico) {
+        this.tecnico = tecnico;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Chamado chamado = (Chamado) o;
+        return id.equals(chamado.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
